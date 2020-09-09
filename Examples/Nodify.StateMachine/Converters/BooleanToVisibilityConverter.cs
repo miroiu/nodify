119cdcfa50e00694957a3dae230a031560c75ed6
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2020-09-09 23:54:26 +0300

    Smart boolean to visibility converter

--- Examples/Nodify.StateMachine/Converters/BooleanToVisibilityConverter.cs
+++ Examples/Nodify.StateMachine/Converters/BooleanToVisibilityConverter.cs
@@ -2,18 +2,20 @@
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace Nodify.StateMachine
 {
-    public class BooleanToVisibilityConverter : IValueConverter
+    public class BooleanToVisibilityConverter : MarkupExtension, IValueConverter
     {
+        public Visibility FalseVisibility { get; set; } = Visibility.Collapsed;
         public bool Negate { get; set; }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool b)
             {
-                return (Negate ? !b : b) ? Visibility.Visible : Visibility.Collapsed;
+                return (Negate ? !b : b) ? Visibility.Visible : FalseVisibility;
             }
 
             return value;
@@ -21,5 +23,7 @@ namespace Nodify.StateMachine
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => value is Visibility v && v == Visibility.Visible;
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
     }
 }
