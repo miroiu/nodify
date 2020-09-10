5666d8732ad22f84b6f462399d1eab30eeada00e
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2020-09-11 02:22:14 +0300

    [StateMachine] Condition viewmodel

---
+++ Examples/Nodify.StateMachine/ConditionViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Nodify.StateMachine
+{
+    public class ConditionViewModel : ObservableObject
+    {
+        private string? _name;
+        public string? Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+    }
+}
