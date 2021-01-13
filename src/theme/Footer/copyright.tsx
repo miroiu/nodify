f092e31750033f441abeb3aaf9d4a35b5192a998
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2021-01-13 14:49:38 +0200

    Might be a Docusaurus bug...

--- src/theme/Footer/copyright.tsx
+++ src/theme/Footer/copyright.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import Link from '@docusaurus/Link';
 import './styles.css';
 
 export const Copyright = () => {
 	return (
 		<div>
 			<div className="copyright-color">
-				Copyright © {new Date().getFullYear()} Emanuel Miroiu.
+				Copyright © {new Date().getFullYear()} Emanuel Miroiu
 			</div>
 			<div className="additionalCopyright">
 				Built with{' '}
-				<a href="https://v2.docusaurus.io/" target="_blank">
+				<Link href="https://v2.docusaurus.io/">
 					<span className="docusaurus-green">Docusaurus</span>
-				</a>{' '}
+				</Link>{' '}
 				· Circuit background by{' '}
-				<a href="https://www.heropatterns.com/" target="_blank">
+				<Link href="https://www.heropatterns.com/">
 					<span className="heropatterns-purple">Steve Schoger</span>
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
