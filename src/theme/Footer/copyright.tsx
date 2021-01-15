8711172bc503608362f412bb47ed3dc83397fb65
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2021-01-15 12:27:47 +0200

    Try to fix copyright links

--- src/theme/Footer/copyright.tsx
+++ src/theme/Footer/copyright.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 import './styles.css';
 
+const DocusaurusLink = () => {
+	return (
+		<Link href="https://v2.docusaurus.io/">
+			<span className="docusaurus-green">Docusaurus</span>
+		</Link>
+	);
+};
+
+const HeroPatternsLink = () => {
+	return (
+		<Link href="https://www.heropatterns.com/">
+			<span className="heropatterns-purple">Steve Schoger</span>
+		</Link>
+	);
+};
+
 export const Copyright = () => {
 	return (
 		<div>
@@ -9,14 +25,8 @@ export const Copyright = () => {
 				Copyright © {new Date().getFullYear()} Emanuel Miroiu
 			</div>
 			<div className="additionalCopyright">
-				Built with{' '}
-				<Link href="https://v2.docusaurus.io/">
-					<span className="docusaurus-green">Docusaurus</span>
-				</Link>{' '}
-				· Circuit background by{' '}
-				<Link href="https://www.heropatterns.com/">
-					<span className="heropatterns-purple">Steve Schoger</span>
-				</Link>
+				Built with <DocusaurusLink /> · Circuit background by{' '}
+				<HeroPatternsLink />
 			</div>
 		</div>
 	);
