08b7665203d22209c652af12300e208db33b15e7
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2021-01-21 18:42:06 +0200

    Update xmlns namespace mapping

--- Nodify/Properties/AssemblyInfo.cs
+++ Nodify/Properties/AssemblyInfo.cs
@@ -4,7 +4,9 @@ using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.ExternalAssembly, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://miroiu.github.io/winfx/xaml/nodify", "Nodify")]
+[assembly: XmlnsDefinition("https://miroiu.github.io/nodify", "Nodify")]
 [assembly: XmlnsPrefix("http://miroiu.github.io/winfx/xaml/nodify", "nodify")]
+[assembly: XmlnsPrefix("https://miroiu.github.io/nodify", "nodify")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("f70fca9c-3224-4b0b-a97a-bde5a92cbf08")]
