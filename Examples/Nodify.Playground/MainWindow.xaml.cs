79a8be7e1a5c9f8dbe28d128159c18458fd75b84
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2021-07-27 16:54:56 +0300

    [Playground] Add BringIntoView example

--- Examples/Nodify.Playground/MainWindow.xaml.cs
+++ Examples/Nodify.Playground/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Nodify.Playground
 {
@@ -7,9 +8,26 @@ namespace Nodify.Playground
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Random _rand = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BringIntoView_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is PlaygroundViewModel model)
+            {
+                NodifyObservableCollection<NodeViewModel> nodes = model.GraphViewModel.Nodes;
+                int index = _rand.Next(nodes.Count);
+
+                if (nodes.Count > index)
+                {
+                    NodeViewModel node = nodes[index];
+                    EditorCommands.BringIntoView.Execute(node.Location, EditorView.Editor);
+                }
+            }
         }
     }
 }
