f44044e88875751ed6a1a284d40ae632bf905b1b
MiroiuGabriel <miroiugabriel1231@gmail.com>
2022-09-27 19:54:15 +0300

    Front page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import './App.css';
+import { GithubButton } from './components';
 
 function App() {
-  return (
-    <main className="max-w-screen-xl">
-      <div>
-        <h1>Build node-based editors quickly, focus on your business logic.</h1>
-        <button>Get started</button> Star 463
-      </div>
-      <div>Logo</div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="max-w-screen-lg mx-auto">
+				<section className="h-screen flex flex-col items-center justify-center gap-8">
+					<div className="flex gap-20">
+						<div className="flex flex-col gap-16">
+							<h1 className="text-6xl tracking-tight font-extrabold">
+								Build{' '}
+								<span className="text-primary">node-based</span>{' '}
+								editors quickly, focus on your business{' '}
+								<span className="text-primary">logic</span>.
+							</h1>
+
+							<div className="flex gap-6 items-center">
+								<a
+									className="bg-primary hover:bg-primary-darker transition-colors duration-200 ease-in-out py-4 px-6 font-bold text-3xl uppercase rounded-md"
+									href="https://github.com/miroiu/nodify/wiki/Getting-Started"
+								>
+									Get started
+								</a>
+								<GithubButton owner="miroiu" repo="nodify" />
+							</div>
+						</div>
+
+						<img
+							src="logo.svg"
+							className="w-64 h-64 hover:scale-105 transition-all duration-200 ease-in-out"
+						/>
+					</div>
+				</section>
+			</div>
+		</main>
+	);
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#fd5618',
+				'primary-darker': '#e94102',
+			},
+			backgroundImage: {
+				'grid-pattern':
+					"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(241 245 249 / 0.03)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e",
+			},
+		},
+	},
+	plugins: [],
 };
