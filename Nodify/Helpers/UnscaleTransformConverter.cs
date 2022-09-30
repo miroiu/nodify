90d0640e99331c9f442d6be75246377d04f947f3
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2022-09-30 23:20:59 +0300

    Fix selection rectangle scaling with viewport zoom

---
+++ Nodify/Helpers/UnscaleTransformConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Nodify
+{
+    internal class UnscaleTransformConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transform result = (Transform)((TransformGroup)value).Children[0].Inverse;
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+    internal class UnscaleDoubleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = (double)values[0] * (double)values[1];
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
