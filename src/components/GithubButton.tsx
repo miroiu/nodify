19d589a24d63f611ff4a21a99cd2b0cedce732bf
MiroiuGabriel <62898193+MiroiuGabriel@users.noreply.github.com>
2022-10-03 22:56:35 +0300

    Front page design (#41)
    * Front page design
    
    * Prototype for showcase and feature sections
    
    * Format files
    
    * Cards
    
    * Feature description
    
    * Responsive design
    
    * Add Nodify title
    
    * Lighthouse fixes
    
    Co-authored-by: Emanuel Miroiu <miroiu.emanuel@yahoo.com>

---
+++ src/components/GithubButton.tsx
@@ -0,0 +1,30 @@
+import { type FC, useEffect, useState } from 'react';
+
+type GithubButtonProps = {
+  owner: string;
+  repo: string;
+};
+
+export const GithubButton: FC<GithubButtonProps> = ({ owner, repo }) => {
+  const [stars, setStars] = useState<number>();
+
+  useEffect(() => {
+    const getCount = async () => {
+      const res = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
+      const data = await res.json();
+      setStars(data.stargazers_count);
+    };
+    getCount();
+  }, []);
+
+  return (
+    <a
+      className="flex bg-white hover:bg-slate-100 transition-colors duration-200 ease-in-out w-fit text-black px-3 py-2 rounded-full gap-2 cursor-pointer font-medium"
+      href="https://github.com/miroiu/nodify"
+    >
+      <img src="img/github_logo.svg" className="w-6 h-6" alt="Github" />
+      Star
+      <span className="border-l min-w-[45px] px-2">{stars}</span>
+    </a>
+  );
+};
