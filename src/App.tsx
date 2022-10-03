2f35cbe999a315650bc5accee352f4209c09729d
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2022-10-03 20:55:05 +0300

    Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,5 @@
+import { apps } from './apps';
 import { GithubButton } from './components';
-
-export type ApplicationInfo = {
-  title: string;
-  preview: any;
-  website?: string; // optional
-  source?: string; // optional
-  description?: string; // optional
-  category?: 'example-app'; // optional
-};
-
-export const apps: ApplicationInfo[] = [
-  {
-    title: 'State Machine',
-    preview: 'img/example-state-machine.png',
-    description: 'Lorem ipsum isidor asdmer asdfasd mamer adxfae msafm',
-    website: 'https://asd.com',
-    source:
-      'https://github.com/miroiu/nodify/tree/master/Examples/Nodify.StateMachine',
-    category: 'example-app',
-  },
-  {
-    title: 'Calculator',
-    preview: 'img/example-calculator.png',
-    source:
-      'https://github.com/miroiu/nodify/tree/master/Examples/Nodify.Calculator',
-    category: 'example-app',
-  },
-  {
-    title: 'Playground',
-    preview: 'img/example-playground.png',
-    source:
-      'https://github.com/miroiu/nodify/tree/master/Examples/Nodify.Playground',
-    category: 'example-app',
-  },
-];
 
 const features = [
   {
@@ -41,30 +7,23 @@ const features = [
     description: 'Designed from the ground up to work with MVVM',
   },
   {
-    title: 'Built for MVMM',
-    description: 'Designed from the ground up to work with MVVM',
+    title: 'High performance',
+    description: 'Optimized for interaction with hundreds of nodes at once',
   },
   {
-    title: 'Built for MVMM',
-    description: 'Designed from the ground up to work with MVVM',
+    title: 'Customizable',
+    description:
+      'Use the built-in themes or create your own with the power of XAML',
   },
   {
-    title: 'Built for MVMM',
-    description: 'Designed from the ground up to work with MVVM',
-  },
-  {
-    title: 'Built for MVMM',
-    description: 'Designed from the ground up to work with MVVM',
-  },
-  {
-    title: 'Built for MVMM',
-    description: 'Designed from the ground up to work with MVVM',
+    title: 'Featureful',
+    description: 'Fully featured navigation, zoom and area selection system',
   },
 ];
 
 function App() {
   return (
-    <main>
+    <div>
       <div className="max-w-screen-lg mx-auto">
         <section className="h-screen flex flex-col items-center justify-center gap-8 relative">
           <div className="flex gap-20">
@@ -77,7 +36,7 @@ function App() {
 
               <div className="flex gap-6 items-center">
                 <a
-                  className="bg-primary hover:bg-primary-darker transition-colors duration-200 ease-in-out py-4 px-6 font-bold text-3xl uppercase rounded-md"
+                  className="bg-primary hover:bg-primary-darker transition-colors duration-200 ease-in-out py-4 px-6 font-bold text-3xl uppercase rounded-lg shadow-sm"
                   href="https://github.com/miroiu/nodify/wiki/Getting-Started"
                 >
                   Get started
@@ -109,50 +68,93 @@ function App() {
         </section>
         <section
           id="showcase"
-          className="scroll-m-4 mt-28 grid grid-cols-3 gap-8"
+          className="scroll-m-4 pt-28 flex flex-col gap-16"
         >
-          <div className="flex flex-col gap-8 col-span-2">
-            {apps.map(app => (
-              <div className="grid grid-cols-2 shadow-2xl">
-                <img
-                  src={app.preview}
-                  alt={app.title}
-                  className="w-full h-48 rounded-tl-md rounded-bl-md"
-                />
-                <div className="bg-card backdrop-blur flex flex-col p-4 rounded-tr-md rounded-br-md">
-                  <h3 className="text-primary text-2xl font-extrabold">
-                    {app.title}
-                  </h3>
-                  {app.description && <p className="">{app.description}</p>}
-                  <div className="flex gap-3 mt-auto">
-                    {app.source && (
-                      <a className="flex items-center justify-center grow border p-1 font-bold rounded-md border-primary hover:bg-primary transition-colors duration-200 ease-in-out">
-                        Source
-                      </a>
-                    )}
-                    {app.website && (
-                      <a className="flex items-center justify-center grow border p-1 font-bold rounded-md border-secondary hover:bg-secondary transition-colors duration-200 ease-in-out">
-                        Visit
-                      </a>
-                    )}
-                  </div>
+          <main className="grid grid-cols-4 gap-6">
+            {features.map(feature => (
+              <div className="relative px-2 py-4 rounded-lg bg-gradient-to-r from-primary-darker to-secondary shadow-lg flex justify-center items-center group">
+                <h4 className="text-xl font-semibold uppercase group-hover:opacity-0">
+                  {feature.title}
+                </h4>
+                <div className="absolute inset-0 flex items-center text-center">
+                  <p className="text-sm font-semibold opacity-0 group-hover:opacity-100">
+                    {feature.description}
+                  </p>
                 </div>
               </div>
             ))}
-          </div>
-          <div className="h-full w-full flex flex-col rounded-md gap-4 bg-card">
-            {features.map(feature => (
-              <div className="p-4">
-                <h4 className="text-2xl font-semibold">{feature.title}</h4>
-                <p>{feature.description}</p>
+          </main>
+
+          <div className="grid grid-cols-2 gap-8">
+            {apps.map(app => (
+              <div className="rounded-lg bg-gradient-to-t from-secondary via-secondary to-primary p-1 shadow-2xl overflow-hidden flex flex-col">
+                <div className="relative group">
+                  <img
+                    src={app.preview}
+                    alt={app.title}
+                    className="w-full h-64 object-center object-cover rounded-t-md"
+                  />
+                  {app.website && (
+                    <div>
+                      <span className="flex h-4 w-4 absolute top-2 right-2 group-hover:opacity-0 transition-opacity duration-200">
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-4 w-4 bg-primary"></span>
+                      </span>
+                      <a
+                        href={app.website}
+                        className="absolute top-0 right-0 w-full h-full [clip-path:circle(0%_at_100%_0)] group-hover:[clip-path:circle(150%_at_100%_0)] motion-safe:transition-all duration-200 bg-secondary opacity-90 flex justify-center items-center"
+                      >
+                        <img
+                          src="img/external-link.svg"
+                          className="w-12 h-12"
+                        />
+                      </a>
+                    </div>
+                  )}
+                </div>
+                <div className="bg-card backdrop-blur flex flex-col p-4 rounded-tr-md rounded-br-md grow">
+                  <h3 className="text-xl font-extrabold mb-2">{app.title}</h3>
+                  {app.description && <p>{app.description}</p>}
+                </div>
               </div>
             ))}
+            <div className="rounded-lg bg-gradient-to-t from-secondary via-secondary to-primary p-1 shadow-2xl overflow-hidden flex flex-col">
+              <a
+                href="https://github.com/miroiu/nodify/edit/docs/src/apps.ts"
+                className="h-full bg-secondary opacity-90 flex justify-center items-center group"
+              >
+                <img
+                  src="img/plus.svg"
+                  className="w-16 h-16 group-hover:w-20 group-hover:h-20 transition-all"
+                />
+              </a>
+            </div>
           </div>
         </section>
       </div>
-      <footer className="flex flex-col gap-4 justify-center items-center py-12 mt-40 bg-card">
+      <footer className="flex flex-col gap-4 justify-center items-center py-12 mt-32 bg-card">
         <div className="flex gap-2 font-semibold uppercase">
-          <a>Docs</a>·<a>Github</a>·<a>Nuget</a>
+          <a
+            href="https://github.com/miroiu/nodify/wiki"
+            className="hover:text-primary-darker"
+          >
+            Docs
+          </a>
+          ·
+          <a
+            href="https://github.com/miroiu/nodify"
+            className="hover:text-primary-darker"
+          >
+            Github
+          </a>
+          ·
+          <a
+            href="https://www.nuget.org/packages/Nodify"
+            target="_blank"
+            className="hover:text-primary-darker"
+          >
+            Nuget
+          </a>
         </div>
         <p className="text-primary">Copyright © 2022 Emanuel Miroiu</p>
         <h6 className="text-sm">
@@ -166,7 +168,7 @@ function App() {
           </a>
         </h6>
       </footer>
-    </main>
+    </div>
   );
 }
 
---
+++ src/apps.ts
@@ -0,0 +1,37 @@
+export type ApplicationInfo = {
+  title: string;
+  preview: any;
+  website?: string; // optional
+  description: string;
+  category?: 'example-app'; // optional
+};
+
+export const apps: ApplicationInfo[] = [
+  {
+    title: 'State Machine',
+    preview: 'img/example-state-machine.png',
+    description:
+      'A state machine application example demonstrating the usage of StateNode and Connection.OffsetMode',
+    website:
+      'https://github.com/miroiu/nodify/tree/master/Examples/Nodify.StateMachine',
+    category: 'example-app',
+  },
+  {
+    title: 'Calculator',
+    preview: 'img/example-calculator.png',
+    description:
+      'A calculator application example showcasing customized nodes and nested graph editors',
+    website:
+      'https://github.com/miroiu/nodify/tree/master/Examples/Nodify.Calculator',
+    category: 'example-app',
+  },
+  {
+    title: 'Playground',
+    preview: 'img/example-playground.png',
+    description:
+      'A playground application where you can test all the available settings',
+    website:
+      'https://github.com/miroiu/nodify/tree/master/Examples/Nodify.Playground',
+    category: 'example-app',
+  },
+];
