401260172bc2d4d883e0342e84edcc46f5a3e662
Riju-bak <defaltsith@gmail.com>
2023-06-28 11:50:22 -0400

    SettingsView.xaml.cs cleanup

--- Examples/Nodify.Playground/SettingsView.xaml.cs
+++ Examples/Nodify.Playground/SettingsView.xaml.cs
@@ -7,13 +7,12 @@ namespace Nodify.Playground
     public partial class SettingsView : UserControl
     {
         public static readonly DependencyProperty ItemsProperty =
-            DependencyProperty.Register("Items", typeof(IEnumerable<ISettingViewModel>), typeof(SettingsView),
-                new PropertyMetadata(null));
+            DependencyProperty.Register(nameof(Items), typeof(IEnumerable<ISettingViewModel>), typeof(SettingsView));
 
         public IEnumerable<ISettingViewModel> Items
         {
-            get { return (IEnumerable<ISettingViewModel>)GetValue(ItemsProperty); }
-            set { SetValue(ItemsProperty, value); }
+            get => (IEnumerable<ISettingViewModel>)GetValue(ItemsProperty);
+            set => SetValue(ItemsProperty, value);
         }
 
         public SettingsView()
