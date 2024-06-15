ae700b2ffc0de0676bab92385b03fd11aef34ece
Emanuel Miroiu <miroiu.emanuel@gmail.com>
2024-06-15 20:37:19 +0300

    Add new built-in connection type: StepConnection (#109)

---
+++ Examples/Nodify.Playground/Converters/FlowToConnectorPositionConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Nodify.Playground
+{
+    public class FlowToConnectorPositionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ConnectorViewModel connector)
+            {
+                if (connector.Node.Orientation == Orientation.Horizontal)
+                {
+                    return connector.Flow == ConnectorFlow.Output
+                        ? ConnectorPosition.Right
+                        : ConnectorPosition.Left;
+                }
+
+                return connector.Flow == ConnectorFlow.Output
+                    ? ConnectorPosition.Bottom
+                    : ConnectorPosition.Top;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
