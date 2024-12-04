4979e1e07f60c1ba79b1669af52db2b1d157c1f0
miroiu <miroiu.emanuel@gmail.com>
2024-12-04 23:10:04 +0200

    Add a custom MouseGesture with support for key combinations

--- Nodify/Helpers/MultiGesture.cs
+++ Nodify/Helpers/MultiGesture.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Linq;
+using System.Windows.Input;
 
 namespace Nodify
 {
@@ -101,10 +103,113 @@ namespace Nodify
         public static implicit operator InputGestureRef(MouseGesture gesture)
             => new InputGestureRef { Value = gesture };
 
+        public static implicit operator InputGestureRef(System.Windows.Input.MouseGesture gesture)
+            => new InputGestureRef { Value = gesture };
+
         public static implicit operator InputGestureRef(KeyGesture gesture)
             => new InputGestureRef { Value = gesture };
 
         public static implicit operator InputGestureRef(MultiGesture gesture)
             => new InputGestureRef { Value = gesture };
+    }
+
+    /// <summary>
+    /// Represents a mouse gesture that optionally includes a specific key press as part of the gesture.
+    /// </summary>
+    public sealed class MouseGesture : System.Windows.Input.MouseGesture
+    {
+        /// <summary>
+        /// Gets or sets the key that must be pressed to match this gesture.
+        /// </summary>
+        public Key Key { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MouseGesture"/> class with the specified mouse action, modifier keys, and a specific key.
+        /// </summary>
+        /// <param name="action">The action associated with this gesture.</param>
+        /// <param name="modifiers">The modifiers associated with this gesture.</param>
+        /// <param name="key">The key required to match the gesture.</param>
+        public MouseGesture(MouseAction action, ModifierKeys modifiers, Key key) : base(action, modifiers)
+        {
+            Key = key;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MouseGesture"/> class with the specified mouse action and key.
+        /// </summary>
+        /// <param name="action">The action associated with this gesture.</param>
+        /// <param name="key">The key required to match the gesture.</param>
+        public MouseGesture(MouseAction action, Key key) : base(action)
+        {
+            Key = key;
+        }
+
+        /// <inheritdoc />
+        public MouseGesture(MouseAction action, ModifierKeys modifiers) : base(action, modifiers)
+        {
+        }
+
+        /// <inheritdoc />
+        public MouseGesture(MouseAction action) : base(action)
+        {
+        }
+
+        /// <inheritdoc />
+        public MouseGesture()
+        {
+        }
+
+        /// <inheritdoc />
+        public override bool Matches(object targetElement, InputEventArgs inputEventArgs)
+        {
+            if (inputEventArgs is MouseButtonEventArgs || inputEventArgs is MouseWheelEventArgs)
+            {
+                return base.Matches(targetElement, inputEventArgs) && MatchesKeyboard();
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks whether the required key is pressed or no keys are pressed when <see cref="Key"/> is <see cref="Key.None"/>.
+        /// </summary>
+        private bool MatchesKeyboard()
+        {
+            if (Key is Key.None)
+            {
+                return !IsAnyKeyPressed();
+            }
+
+            return Keyboard.GetKeyStates(Key).HasFlag(KeyStates.Down);
+        }
+
+        private static readonly Key[] _allKeys = GetValidKeys();
+
+        private static Key[] GetValidKeys()
+        {
+            var excludedKeys = new[]
+            {
+                Key.LeftCtrl, Key.RightCtrl,
+                Key.LeftShift, Key.RightShift,
+                Key.LeftAlt, Key.RightAlt,
+                Key.LWin, Key.RWin,
+                Key.None
+            };
+
+#if NET5_0_OR_GREATER
+            return Enum.GetValues<Key>()
+#else
+            return Enum.GetValues(typeof(Key))
+                .Cast<Key>()
+#endif
+                .Where(key => !excludedKeys.Contains(key))
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Determines whether any key (excluding modifiers) is currently pressed.
+        /// </summary>
+        private static bool IsAnyKeyPressed()
+            => _allKeys.Any(Keyboard.IsKeyDown);
     }
 }
