ccf13c6d591cc3b5f2c6632acd0cf74a237bb9ec
miroiu <miroiu.emanuel@gmail.com>
2025-06-19 19:45:21 +0300

    Maybe?

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,12 @@ import type { MiddlewareHandler } from 'astro';
 /**
  * This middleware converts the pathname to lowercase and adds .html to the final path for documentation pages and redirects the request to the URL with a 301 status code.
  */
-export const onRequest: MiddlewareHandler = async ({ request }, next) => {
-  const url = new URL(request.url);
+export const onRequest: MiddlewareHandler = async (context, next) => {
+  const url = new URL(context.request.url);
 
   if (url.pathname.includes('wiki/') && !url.pathname.includes('.html')) {
     const redirectUrl = `${url.pathname.toLocaleLowerCase()}.html`;
-    return new Response(null, {
-      status: 301,
-      headers: {
-        Location: redirectUrl,
-      },
-    });
+    return context.rewrite(new Request(new URL(redirectUrl, url.origin)));
   }
 
   return next();
