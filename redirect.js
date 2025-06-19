377667ae08937c67d22534de3852fdbcd80595af
miroiu <miroiu@users.noreply.github.com>
2025-06-19 17:45:37 +0000

    deploy: b58bfe95f33cd6cbf10948fcef7f3fe0847c67df

--- redirect.js
+++ redirect.js
@@ -2,5 +2,5 @@ const url = window.location;
 
 if (url.pathname.includes('wiki/') && !url.pathname.includes('.html')) {
   const redirectUrl = `${url.pathname.toLocaleLowerCase()}.html`;
-  window.location.href = redirectUrl;
+  window.location.replace(redirectUrl);
 }
