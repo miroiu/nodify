02b5c23347c90aff14a3b1684f3dcbeaba95dbcd
miroiu <miroiu.emanuel@gmail.com>
2025-06-19 18:47:07 +0300

    Disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,24 @@
-import type { MiddlewareHandler } from 'astro';
+// import type { MiddlewareHandler } from 'astro';
 
-/**
- * This middleware converts the pathname to lowercase and removes any trailing slashes for documentation pages and redirects the request to the normalized URL with a 301 status code.
- */
-export const onRequest: MiddlewareHandler = async ({ request }, next) => {
-  const url = new URL(request.url);
+// /**
+//  * This middleware converts the pathname to lowercase and removes any trailing slashes for documentation pages and redirects the request to the normalized URL with a 301 status code.
+//  */
+// export const onRequest: MiddlewareHandler = async ({ request }, next) => {
+//   const url = new URL(request.url);
 
-  if (url.pathname.includes('/wiki')) {
-    const normalizedPath = url.pathname.toLowerCase().replace(/\/$/, '');
+//   if (url.pathname.includes('/wiki')) {
+//     const normalizedPath = url.pathname.toLowerCase().replace(/\/$/, '');
 
-    if (url.pathname !== normalizedPath) {
-      const redirectUrl = `${normalizedPath}${url.search}`;
-      return new Response(null, {
-        status: 301,
-        headers: {
-          Location: redirectUrl,
-        },
-      });
-    }
-  }
+//     if (url.pathname !== normalizedPath) {
+//       const redirectUrl = `${normalizedPath}${url.search}`;
+//       return new Response(null, {
+//         status: 301,
+//         headers: {
+//           Location: redirectUrl,
+//         },
+//       });
+//     }
+//   }
 
-  return next();
-};
+//   return next();
+// };
