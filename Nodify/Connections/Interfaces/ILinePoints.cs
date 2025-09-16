485cc510c2068f6f7b863dadf78f55fb74fbd344
ToCheckOn <staszek.gaming@gmail.com>
2025-09-16 11:29:50 +0200

    Span not needed and summary

--- Nodify/Connections/Interfaces/ILinePoints.cs
+++ Nodify/Connections/Interfaces/ILinePoints.cs
@@ -5,14 +5,21 @@ namespace Nodify
 {
     public interface ILinePoints<TPoints> where TPoints : struct, IPointTuple
     {
+        /// <summary>
+        /// Allows to call base class version of GetLinePoints
+        /// example:
+        /// BaseClass implements ILinePoints<Point2>
+        /// DerivedClass implements ILinePoints<Point2>
+        /// call '((ILinePoints<Point2>)(BaseClass)derivedClass).GetLinePoints(source, target)'
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
         TPoints GetLinePoints(in Point source, in Point target);
     }
 
     public interface IPointTuple
     {
-        int Length { get; }
-        Point this[int index] { get; }
-        ReadOnlySpan<Point> AsSpan();
     }
 
     public readonly struct Point2 : IPointTuple
@@ -23,10 +30,6 @@ namespace Nodify
         public Point2(Point p0, Point p1) => (P0, P1) = (p0, p1);
 
         public void Deconstruct(out Point p0, out Point p1) => (p0, p1) = (P0, P1);
-
-        public int Length => 2;
-        public Point this[int i] => i switch { 0 => P0, 1 => P1, _ => throw new IndexOutOfRangeException() };
-        public ReadOnlySpan<Point> AsSpan() => new[] { P0, P1 };
 
         public static implicit operator (Point, Point)(Point2 p) => (p.P0, p.P1);
         public static implicit operator Point2((Point, Point) t) => new(t.Item1, t.Item2);
@@ -42,10 +45,6 @@ namespace Nodify
 
         public void Deconstruct(out Point p0, out Point p1, out Point p2) => (p0, p1, p2) = (P0, P1, P2);
 
-        public int Length => 3;
-        public Point this[int i] => i switch { 0 => P0, 1 => P1, 2 => P2, _ => throw new IndexOutOfRangeException() };
-        public ReadOnlySpan<Point> AsSpan() => new[] { P0, P1, P2 };
-
         public static implicit operator (Point, Point, Point)(Point3 p) => (p.P0, p.P1, p.P2);
         public static implicit operator Point3((Point, Point, Point) t) => new(t.Item1, t.Item2, t.Item3);
     }
@@ -60,10 +59,6 @@ namespace Nodify
         public Point4(Point p0, Point p1, Point p2, Point p3) => (P0, P1, P2, P3) = (p0, p1, p2, p3);
 
         public void Deconstruct(out Point p0, out Point p1, out Point p2, out Point p3) => (p0, p1, p2, p3) = (P0, P1, P2, P3);
-
-        public int Length => 4;
-        public Point this[int i] => i switch { 0 => P0, 1 => P1, 2 => P2, 3 => P3, _ => throw new IndexOutOfRangeException() };
-        public ReadOnlySpan<Point> AsSpan() => new[] { P0, P1, P2, P3 };
 
         public static implicit operator (Point, Point, Point, Point)(Point4 p) => (p.P0, p.P1, p.P2, p.P3);
         public static implicit operator Point4((Point, Point, Point, Point) t) => new(t.Item1, t.Item2, t.Item3, t.Item4);
